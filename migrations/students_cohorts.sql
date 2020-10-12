0524a80abd89150440828f8785da9cb1ac87ec08
Xiaowen Zheng <zxw880507@gmail.com>
2020-10-12 20:12:17 +0000

    first commit

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,15 @@
+CREATE TABLE cohorts (id SERIAL PRIMARY KEY NOT NULL, 
+name VARCHAR(255) NOT NULL, 
+start_date DATE, 
+end_date DATE);
+
+CREATE TABLE students (id SERIAL PRIMARY KEY NOT NULL, 
+name VARCHAR(255) NOT NULL, 
+age INTEGER, email VARCHAR(255), 
+phone VARCHAR(255), 
+github TEXT, 
+start_date DATE, 
+end_date DATE, 
+cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE);
+
+
