03457194892412bc084bbbbc54796c3a021616c4
Calvin Zheng <calvinzheng@Calvins-MBP.hitronhub.home>
2020-10-12 18:20:36 -0600

    Adding the third queries

---
+++ 3_queries_group-by/low_average_completion_time.sql
@@ -0,0 +1,12 @@
+SELECT students.name AS student,
+AVG(assignment_submissions.duration) AS average_assignment_duration,
+AVG(assignments.duration) AS average_estimated_duration
+FROM assignment_submissions
+JOIN students
+ON student_id = students.id
+JOIN assignments
+ON assignment_id = assignments.id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING AVG(assignment_submissions.duration) < AVG(assignments.duration)
+ORDER BY average_assignment_duration;
---
+++ 3_queries_group-by/total_assignment_submissions.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name AS cohort, COUNT(assignment_submissions.*) AS total_submissions
+FROM cohorts
+JOIN students ON cohorts.id = students.cohort_id
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+GROUP BY cohorts.name
+ORDER BY total_submissions DESC;
